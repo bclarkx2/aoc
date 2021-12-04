2f9550ae35cbfdc6ab89aa700ad654dd212f2894
Brian <bclark@gmail.com>
2021-12-03 21:52:35 -0500

    Add script to generate new puzzle template

---
+++ template/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/bclarkx2/aoc"
+)
+
+type solver struct{}
+
+func (s *solver) Name() string {
+	return ""
+}
+
+func (s *solver) Solve1(input []string) (int, error) {
+	return 1, nil
+}
+
+func (s *solver) Solve2(input []string) (int, error) {
+	return 2, nil
+}
+
+func main() {
+	aoc.Run(aoc.ParseInputFile(), &solver{})
+}
